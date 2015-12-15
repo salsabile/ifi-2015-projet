9b2f6ca36df8b1929d24f207cdbf1ebb4929001a
jordann <jordann.gamba@gmail.com>
2015-12-15 01:32:09 +0100

    Un message contenant le fichier a telechargé s'affiche lorsqu'on envoie un message au serveur

--- src/main/java/ifi2015/ifi_2015_projet/DocumentController.java
+++ src/main/java/ifi2015/ifi_2015_projet/DocumentController.java
@@ -10,12 +10,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,28 +24,35 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class DocumentController {
 
-		
 	@RequestMapping(value = "/document", method=RequestMethod.POST)
 	public String sendDocument(@RequestParam("file") MultipartFile file, Model model) throws IOException {
 		
 		String filename = file.getOriginalFilename();
-		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.getMessageConverters().add(new FormHttpMessageConverter());
-		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		String name = filename.split("\\.")[0];
+		String extension = filename.split("\\.")[1];
+		String messagePieceJointe = "Je vous partage ce fichier <a href=/documents/"+filename+">"+filename+"</a> #File";
 		
+		File fileTemporaire = File.createTempFile(name, extension);
+		FileCopyUtils.copy(file.getInputStream(), new FileOutputStream(fileTemporaire));
+		LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+		map.add("file", new FileSystemResource(fileTemporaire));
+		
+		RestTemplate restTemplate = new RestTemplate();
+
 		HttpHeaders requestHttpHeader = new HttpHeaders();
 		requestHttpHeader.setContentType(MediaType.MULTIPART_FORM_DATA);
 		requestHttpHeader.setContentDispositionFormData("file", filename);
 		
-		LinkedMultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
-		File tempFile = File.createTempFile("xyz", "");
-		FileCopyUtils.copy(file.getInputStream(), new FileOutputStream(tempFile));
-		map.add("file", new FileSystemResource(tempFile));
-		
 		HttpEntity<Object> requestHttpEntity = new HttpEntity<Object>(map, requestHttpHeader);
-		//HttpEntity<String> requestHttpEntity = new HttpEntity<String>("", requestHttpHeader);
-		
 		restTemplate.exchange("http://localhost:9191/documents/"+filename, HttpMethod.POST, requestHttpEntity, String.class);
+		
+		restTemplate = new RestTemplate();
+		requestHttpHeader = new HttpHeaders();
+		requestHttpHeader.setContentType(MediaType.APPLICATION_JSON);
+		Message message = new Message(messagePieceJointe, UserController.name);
+		
+		HttpEntity<String> requestHttpEntityMessage = new HttpEntity<String>("{\"content\":\""+message.getContent()+"\", \"login\":\""+UserController.username+"\" }", requestHttpHeader);
+		restTemplate.exchange("http://localhost:9393/messages/message", HttpMethod.POST, requestHttpEntityMessage, String.class);
 		
 		MessageController.messages = new ArrayList<Message>();
 		MessageController.messages = MessageController.afficherMessage(MessageController.messages, model);
@@ -57,4 +63,18 @@ public class DocumentController {
 		
 		return "index";
 	}
+	
+	@RequestMapping(value = "/document/{file}", method=RequestMethod.GET)
+	public String downloadDocument(@PathVariable("file") String file, Model model) throws IOException {
+		
+		MessageController.messages = new ArrayList<Message>();
+		MessageController.messages = MessageController.afficherMessage(MessageController.messages, model);
+		MessageController.afficherHashtag(MessageController.messages);
+		model.addAttribute("message", new Message());
+		model.addAttribute("name",UserController.name);
+		model.addAttribute("messages", MessageController.messages);
+		
+		return "index";
+	}	
+
 }
